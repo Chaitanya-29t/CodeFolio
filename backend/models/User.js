1d1073f2be335ffc6fb56dfb8e2ea9904eb278e8
Chaitanya <chaitanyatodase78@gmail.com>
2026-09-11 02:41:55 +0530

    Add custom domain support

--- backend/models/User.js
+++ backend/models/User.js
@@ -45,6 +45,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    
+    customDomain: {
+  type: String,
+  default: "",
+},
 
     templateId: {
       type: String,
